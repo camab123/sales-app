1c4725f29980e67bc7dc9fde3f36dc61c2faa847
Cameron Abeel <camabeel@Camerons-MBP.nyc.rr.com>
2025-04-11 15:26:36 -0400

    remove docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# Wait for the database to be ready (using pg_isready or a custom loop)
-until pg_isready -h db -p 5432; do
-    echo "Waiting for the PostgreSQL database at db:5432..."
-    sleep 1
-done
-
 # Run Alembic migrations
 echo "Running migrations..."
 alembic upgrade head
